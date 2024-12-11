457b54cab6b14a6a2775af2bba42c152e55a4871
Valeria Aguilar <aguilarvale690@gmail.com>
2024-12-10 21:32:55 -0600

    nuevo commit

---
+++ src/test/java/com/fca/calidad/funcionales/crudTest.java
@@ -0,0 +1,165 @@
+package com.fca.calidad.funcionales;
+
+
+import java.time.Duration;
+
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class crudTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @Before
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup();
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    baseUrl = "https://mern-crud-mpfr.onrender.com/";
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testCRUD() throws Exception {
+    driver.get("https://mern-crud-mpfr.onrender.com/");
+    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/button")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Elon Musk");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("elonmusk@teslaonly.com");
+    driver.findElement(By.name("age")).click();
+    driver.findElement(By.name("age")).clear();
+    driver.findElement(By.name("age")).sendKeys("20");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[1]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("elonmusk@teslaonly.com");
+    driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/form/button")).click();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    WebElement confirmationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div[2]/form/div[4]/div/div")));
+    assertEquals("Nice one!", confirmationMessage.getText());
+    //ERROR: Caught exception [unknown command []]
+  }
+
+  @Test
+  public void testEditar() throws Exception {
+    driver.get("https://mern-crud-mpfr.onrender.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr[4]/td[5]/button")).click();
+    Thread.sleep(1000);
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Editar");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+    driver.findElement(By.xpath("//i")).click();
+    driver.navigate().refresh();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    WebElement confirmationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/div[2]/table/tbody/tr[1]/td[1]")));
+    assertEquals("Editar", confirmationMessage.getText());
+  }
+
+  @Test
+  public void testDelete() throws Exception {
+	  driver.get("https://mern-crud-mpfr.onrender.com/");
+	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/button")).click();
+	    driver.findElement(By.name("name")).click();
+	    driver.findElement(By.name("name")).clear();
+	    driver.findElement(By.name("name")).sendKeys("Borrar");
+	    driver.findElement(By.name("email")).click();
+	    driver.findElement(By.name("email")).clear();
+	    driver.findElement(By.name("email")).sendKeys("borrar@gmail.com");
+	    driver.findElement(By.name("email")).clear();
+	    driver.findElement(By.name("email")).sendKeys("borrar@gmail.com.mx");
+	    driver.findElement(By.name("age")).click();
+	    driver.findElement(By.name("age")).clear();
+	    driver.findElement(By.name("age")).sendKeys("18");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[2]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Female'])[1]/following::span[1]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+	    driver.findElement(By.xpath("//i")).click();
+	    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+	    driver.findElement(By.xpath("/html/body/div[3]/div/div[3]/button[1]")).click();   
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+	    WebElement confirmationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/div[2]/table/tbody/tr[1]/td[1]")));
+	    assertNotEquals("Borrar", confirmationMessage.getText());
+  }
+
+
+
+
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
